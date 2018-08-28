8d461e9dbec71a4ac8f4a60ba58bb55d0029a2e0
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2018-08-28 12:30:42 +0200

    Added logging, extracted args to its own file

---
+++ src/args.rs
@@ -0,0 +1,44 @@
+use clap::{Arg, App, ArgMatches};
+
+const NAME: &'static str = env!("CARGO_PKG_NAME");
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
+const DESCRIPTION: &'static str = env!("CARGO_PKG_DESCRIPTION");
+
+const DEFAULT_ADDRES: &'static str = "127.0.0.1";
+const DEFAULT_PORT: &'static str = "61000";
+
+pub struct Args {
+    pub interactive: bool,
+    pub addres: String,
+    pub port: String,
+    pub verify_trust: bool,
+    pub command: String
+}
+
+pub fn parse_args() -> Args {
+    let matches = App::new(NAME)
+        .version(VERSION)
+        .author(AUTHORS)
+        .about(DESCRIPTION)
+        .args(&vec![
+            Arg::from_usage("-i --interactive 'Enter interactive mode'").conflicts_with("input"),
+            Arg::from_usage("-a --addres=[addres] 'Golem node's RPC address'"),
+            Arg::from_usage("-p --port=[port] 'Golem node's RPC port'"),
+            Arg::from_usage("-t --verify-trust 'Verify Golem node's certificate'"),
+            Arg::from_usage("<command> 'The command to execute'").required(false),
+        ])
+        .get_matches();
+
+    return matches_to_args(matches);
+}
+
+fn matches_to_args(matches:ArgMatches) -> Args {
+    let interactive = matches.is_present("interactive"); 
+    let addres = matches.value_of("addres").unwrap_or(DEFAULT_ADDRES).to_string();
+    let port = matches.value_of("port").unwrap_or(DEFAULT_PORT).to_string();
+    let verify_trust = matches.is_present("verify-trust");
+    let command = matches.value_of("command").unwrap_or("").to_string();
+
+    return Args{interactive, addres, port, verify_trust, command};
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,22 @@
+#[macro_use]
+extern crate log;
 extern crate clap;
+extern crate env_logger;
+mod args;
 
-use clap::{Arg, App};
+use args::{Args, parse_args};
 
 fn main() {
-    const NAME: &'static str = env!("CARGO_PKG_NAME");
-    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
-    const AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
-    const DESCRIPTION: &'static str = env!("CARGO_PKG_DESCRIPTION");
-    let matches = App::new(NAME)
-        .version(VERSION)
-        .author(AUTHORS)
-        .about(DESCRIPTION)
-        .args(
-            &vec![
-                Arg::from_usage("-i --interactive 'Enter interactive mode'").conflicts_with("input"),
-                Arg::from_usage("-a --addres=[addres] 'Golem node's RPC address'"),
-                Arg::from_usage("-p --port=[port] 'Golem node's RPC port'"),
-                Arg::from_usage("-t --verify-trust 'Verify Golem node's certificate'"),
-                Arg::from_usage("<command> 'The command to execute'").required(false),
-            ]
-        )
-        .get_matches();
+    env_logger::init();
+    trace!("START");
+    debug!("Parsing args...");
+    let a:Args = parse_args();
+    debug!("Interactive mode '{}'", a.interactive.to_string());
+    debug!("addres '{}'", a.addres);
+    debug!("port '{}'", a.port);
+    debug!("verify_trust '{}'", a.verify_trust.to_string());
+    debug!("command '{}'", a.command);
 
-    let interactive = matches.is_present("interactive"); 
-    let addres = matches.value_of("addres").unwrap_or("wss://127.0.0.1");
-    let port = matches.value_of("port").unwrap_or("61000");
-    let verify_trust = matches.is_present("verify-trust");
-    let command = matches.value_of("command").unwrap_or("");
 
-    println!("Interactive mode {}", interactive.to_string());
-    println!("addres {}", addres);
-    println!("port {}", port);
-    println!("verify_trust {}", verify_trust.to_string());
-    println!("command {}", command);
+    trace!("END");
 }
