73209eacb65ff30a0c9cb7e3a584bdf980cdbd5f
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2018-08-29 19:51:45 +0200

    add matching for test commands, they require quotes: ./golemcli "network show" for now

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -6,14 +6,20 @@ mod networkshow;
 use self::setpassword::set_password;
 use self::networkshow::network_show;
 
+const NETWORK_SHOW: &'static str = "network show";
+const ACCOUNT_UNLOCK: &'static str = "account unlock";
+
 pub fn listen_and_fire(mut session:Client, interactive:bool, command:String) {
     debug!("listen_and_fire({}, {}, {})", "session", interactive, command);
     if interactive {
         debug!("Starting interactive mode");
     }
     
-    set_password(&mut session);
-    //network_show(&mut session);
+    match command.as_ref() {
+        ACCOUNT_UNLOCK => set_password(&mut session),
+        NETWORK_SHOW => network_show(&mut session),
+        _ => error!("Unknown command, bye"),
+    }
     let s_res = session.shutdown().unwrap();
     debug!("shutdown_res: {:?}", s_res);
 
